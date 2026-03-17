f08a167207e914b202b7e26a01f90c462c8f535a
BergUetli <berguetli@gmail.com>
2026-03-17 21:33:34 +0530

    Delete push.bat

--- push.bat
+++
@@ -1,7 +0,0 @@
-@echo off
-cd /d C:\Users\Rishi\PROJECTS\RPresearch\deploy
-git init -b main
-git add -A
-git commit -m "Initial deploy of rishiprasad.me portfolio"
-git remote add origin https://github.com/BergUetli/rishiprasad.me.git
-git push -u origin main
